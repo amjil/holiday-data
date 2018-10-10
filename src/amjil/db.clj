c323ebd7127f5955043a453b452d430bddd147bb
sqbh <amjilt@aliyun.com>
2018-10-10 15:55:02 +0800

    delete db name from sql

--- src/amjil/db.clj
+++ src/amjil/db.clj
@@ -19,11 +19,10 @@
 
 
 (defn delete-all [year]
-  ; (jdbc/execute! td ["delete from NGBASS.IOP_calendar_holiday where extract(year from cal_date) = ?" year])
   (jdbc/delete! td :iop_calendar_holiday ["extract(year from cal_date) = ?" year]))
 
 (defn insert-data [data]
   (jdbc/insert-multi! td :iop_calendar_holiday nil data))
 
 (defn query-all [year]
-  (jdbc/query td ["select * from ngbass.iop_calendar_holiday where extract(year from cal_date) = ?" year]))
+  (jdbc/query td ["select * from iop_calendar_holiday where extract(year from cal_date) = ?" year]))
